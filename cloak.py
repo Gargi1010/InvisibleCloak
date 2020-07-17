b2a9242b2cee63b301ce40a9ccd235ef3c8efd3e
Gargi1010 <61877906+Gargi1010@users.noreply.github.com>
2020-07-17 23:17:40 +0530

    Create cloak.py

---
+++ cloak.py
@@ -0,0 +1,48 @@
+import numpy as np
+import time
+import cv2
+cap = cv2.VideoCapture(0)
+time.sleep(2)
+
+background=0
+
+for i in range(30):
+    ret, background = cap.read()
+    
+while(cap.isOpened()):
+    ret, img=cap.read()
+    
+    if not ret:
+        break
+    hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    #HSV VALUES
+    lower_red=np.array([100, 40, 40])
+    upper_red=np.array([100, 255, 255])
+    
+    mask1=cv2.inRange(hsv, lower_red, upper_red) #seperating the cloak part
+    
+    lower_red = np.array([155, 40, 40])
+    upper_red = np.array([180, 255, 255])
+    mask2=cv2.inRange(hsv, lower_red, upper_red)
+    
+    mask1 = mask1 + mask2
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN,
+                             np.ones((3, 3), np.uint8), iterations=2) #reduce noise
+    
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE,
+                             np.ones((3, 3), np.uint8), iterations=1) #INCREASE SMOOTHNESS
+    mask2=cv2.bitwise_not(mask1) #everything exept the cloak
+    
+    res1 = cv2.bitwise_and(background, background, mask=mask1) #used for segmentation of color
+    res2 = cv2.bitwise_and(img, img, mask=mask2)#used to seperate the cloak
+    
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0) #does linear addition
+    
+    cv2.imshow('YAYY!', final_output)
+    k=cv2.waitKey(10)
+    if k == 27:
+        break
+        
+cap.release()
+cv2.destroyAllWindows() 
